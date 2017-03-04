f82d9fbbfa63ce15118e28d8aebb9689f1ebe42a
Christian <morsingnet@gmail.com>
2017-03-03 22:30:17 -0600

    first change in promises

--- quizzes/graders/readyState.js
+++ quizzes/graders/readyState.js
@@ -1,7 +1,8 @@
 try {
-	ready().then(function() {
-		window.dispatchEvent(new CustomEvent('ud-resolved', {'detail': 'passed'}));
-	});
+    ready().then(function() {
+        window.dispatchEvent(new CustomEvent('ud-resolved', { 'detail': 'passed' }));
+    });
 } catch (e) {
-	// don't need to alert students to errors here
+    // don't need to alert students to errors here
+    // New stuff
 }
