3256a98b383f170e36f4a3c170984b9c3b5116f8
Christian Morsing <morsingnet@gmail.com>
2017-03-05 21:52:51 -0600

    test

---
+++ quizzes/graders/readyState.js
@@ -0,0 +1,8 @@
+try {
+    ready().then(function() {
+        window.dispatchEvent(new CustomEvent('ud-resolved', { 'detail': 'passed' }));
+    });
+} catch (e) {
+    // don't need to alert students to errors here
+    // New stuff
+}
